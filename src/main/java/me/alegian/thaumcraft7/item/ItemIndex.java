09613ea8e7971ae8f0c29caa601f084d940beff2
alegian <alegiannx@gmail.com>
2024-01-04 00:27:10 +0200

    custom item class and use handler

--- src/main/java/me/alegian/thaumcraft7/item/ItemIndex.java
+++ src/main/java/me/alegian/thaumcraft7/item/ItemIndex.java
@@ -9,7 +9,7 @@ public class ItemIndex {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Thaumcraft.MODID);
 
     // Creates a new item with the id "examplemod:example_item"
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties());
+    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item");
     public static final DeferredItem<Item> IRON_CAP = ITEMS.registerSimpleItem("iron_cap", new Item.Properties());
-    public static final DeferredItem<Item> IRON_WOOD_WAND = ITEMS.registerSimpleItem("iron_wood_wand", new Item.Properties().stacksTo(1));
+    public static final DeferredItem<Item> IRON_WOOD_WAND = ITEMS.registerItem("iron_wood_wand", WandItem::new);
 }
---
+++ src/main/java/me/alegian/thaumcraft7/item/WandItem.java
@@ -0,0 +1,21 @@
+package me.alegian.thaumcraft7.item;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class WandItem extends Item {
+    public WandItem(Properties p_41383_) {
+        super(p_41383_);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        if(level.isClientSide()) player.sendSystemMessage(Component.literal("use"));
+        return super.use(level, player, hand);
+    }
+}
