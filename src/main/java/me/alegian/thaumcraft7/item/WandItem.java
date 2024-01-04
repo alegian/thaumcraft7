e237ff82c06806b4b33514bded4825785a17b81c
alegian <alegiannx@gmail.com>
2024-01-04 12:52:46 +0200

    continuous right click handler

--- src/main/java/me/alegian/thaumcraft7/item/WandItem.java
+++ src/main/java/me/alegian/thaumcraft7/item/WandItem.java
@@ -3,9 +3,11 @@ package me.alegian.thaumcraft7.item;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 
 public class WandItem extends Item {
@@ -16,6 +18,22 @@ public class WandItem extends Item {
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         if(level.isClientSide()) player.sendSystemMessage(Component.literal("use"));
-        return super.use(level, player, hand);
+        player.startUsingItem(hand);
+        return InteractionResultHolder.success(player.getItemInHand(hand));
+    }
+
+    @Override
+    public void releaseUsing(ItemStack itemStack, Level level, LivingEntity entity, int someDuration) {
+        if(level.isClientSide() && entity instanceof Player) entity.sendSystemMessage(Component.literal("release using"));
+    }
+
+    @Override
+    public int getUseDuration(ItemStack itemStack) {
+        return 72000;
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack itemStack) {
+        return UseAnim.BOW;
     }
 }
