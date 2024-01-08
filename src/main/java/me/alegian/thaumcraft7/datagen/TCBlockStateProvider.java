9be5361f1be3dddf3bdf8f70e4798767683f637d
alegian <alegiannx@gmail.com>
2024-01-08 21:55:19 +0200

    cleaner code

--- src/main/java/me/alegian/thaumcraft7/datagen/TCBlockStateProvider.java
+++ src/main/java/me/alegian/thaumcraft7/datagen/TCBlockStateProvider.java
@@ -3,10 +3,8 @@ package me.alegian.thaumcraft7.datagen;
 import me.alegian.thaumcraft7.Thaumcraft;
 import me.alegian.thaumcraft7.block.BlockIndex;
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
-import net.neoforged.neoforge.client.model.generators.ModelFile;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 public class TCBlockStateProvider extends BlockStateProvider {
@@ -16,22 +14,11 @@ public class TCBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        simpleBlockAndItem(BlockIndex.EXAMPLE_BLOCK.get(), "node");
+        simpleBlockWithItem(BlockIndex.EXAMPLE_BLOCK.get());
     }
 
-    protected void simpleBlockAndItem(Block block) {
+    public void simpleBlockWithItem(Block block) {
         var model = cubeAll(block);
-        simpleBlock(block, model);
-        simpleBlockItem(block, model);
-    }
-
-    protected void simpleBlockAndItem(Block block, ModelFile model) {
-        simpleBlock(block, model);
-        simpleBlockItem(block, model);
-    }
-
-    protected void simpleBlockAndItem(Block block, String id) {
-        var model = models().cubeAll(id, new ResourceLocation(Thaumcraft.MODID, "block/"+id));
         simpleBlock(block, model);
         simpleBlockItem(block, model);
     }
