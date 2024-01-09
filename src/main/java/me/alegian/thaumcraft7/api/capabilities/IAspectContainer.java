d9c9079989f167f576f77355c31b82b015065841
alegian <alegiannx@gmail.com>
2024-01-09 12:25:18 +0200

    improve comment

--- src/main/java/me/alegian/thaumcraft7/api/capabilities/IAspectContainer.java
+++ src/main/java/me/alegian/thaumcraft7/api/capabilities/IAspectContainer.java
@@ -3,8 +3,9 @@ package me.alegian.thaumcraft7.api.capabilities;
 import me.alegian.thaumcraft7.api.aspects.AspectList;
 
 /*
+ * WARNING: this differs from the old Thaumcraft API
  * represents any Block, Item, Entity that contains Aspects that can be scanned with
- * a Thaumometer
+ * a Thaumometer.
  */
 public interface IAspectContainer {
     public AspectList getAspects();
