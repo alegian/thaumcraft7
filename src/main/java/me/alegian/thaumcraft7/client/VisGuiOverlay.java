7aa3eeffdabc0c6fd9116a3680c6184f27427181
alegian <alegiannx@gmail.com>
2024-01-10 13:26:19 +0200

    more gui render logic

--- src/main/java/me/alegian/thaumcraft7/client/VisGuiOverlay.java
+++ src/main/java/me/alegian/thaumcraft7/client/VisGuiOverlay.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.client.gui.overlay.IGuiOverlay;
+import org.joml.Matrix4f;
 
 public class VisGuiOverlay {
     private static final ResourceLocation DISK = new ResourceLocation(Thaumcraft.MODID, "textures/overlay/disk.png");
@@ -17,6 +18,21 @@ public class VisGuiOverlay {
         float scale = 0.15f;
         int width = (int)(screenHeight*scale);
 
-        guiGraphics.blit(DISK, (int) (screenHeight*0.02f), (int) (screenHeight*0.02f), 0, 0, width, width, width, width);
+        guiGraphics.pose().pushPose();
+
+        var translationMatrix = new Matrix4f()
+                .translate(screenHeight*0.02f, screenHeight*0.02f, 0);
+        guiGraphics.pose().mulPoseMatrix(translationMatrix);
+
+        guiGraphics.blit(DISK, 0, 0, 0, 0, width, width, width, width);
+
+        guiGraphics.pose().mulPoseMatrix(rotationMatrix(15));
+        guiGraphics.blit(VIAL, 0, 0, 0, 0, 24, 24, 96, 96);
+
+        guiGraphics.pose().popPose();
     });
+
+    public static Matrix4f rotationMatrix(int deg){
+        return new Matrix4f().rotateZ(deg);
+    }
 }
