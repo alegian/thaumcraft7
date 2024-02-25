b74b22aea730bb86c653cfd7ba4719d0b81e55d2
alegian <alegiannx@gmail.com>
2024-02-25 16:46:30 +0200

    small dry up

--- src/main/java/me/alegian/thaumcraft7/client/texture/TCTextures.java
+++ src/main/java/me/alegian/thaumcraft7/client/texture/TCTextures.java
@@ -2,12 +2,14 @@ package me.alegian.thaumcraft7.client.texture;
 
 public class TCTextures {
     public static final class Thaumonomicon{
-        public static final Texture FRAME = new Texture("textures/gui/thaumonomicon/frame.png", 1024, 512);
-        public static final Texture TAB_BG = new Texture("textures/gui/thaumonomicon/tab_bg.png", 512, 512);
-        public static final Texture NODE = new Texture("textures/gui/thaumonomicon/node.png", 32, 32);
-        public static final Texture ARROW_HEAD = new Texture("textures/gui/thaumonomicon/arrow_head.png", 32, 32);
-        public static final Texture LINE = new Texture("textures/gui/thaumonomicon/line.png", 32, 32);
-        public static final Texture CORNER_1X1 = new Texture("textures/gui/thaumonomicon/corner_1x1.png", 32, 32);
-        public static final Texture CORNER_3X3 = new Texture("textures/gui/thaumonomicon/corner_3x3.png", 96, 96);
+        private static final String CONTEXT = "textures/gui/thaumonomicon/";
+
+        public static final Texture FRAME = new Texture(CONTEXT+"frame.png", 1024, 512);
+        public static final Texture TAB_BG = new Texture(CONTEXT+"tab_bg.png", 512, 512);
+        public static final Texture NODE = new Texture(CONTEXT+"node.png", 32, 32);
+        public static final Texture ARROW_HEAD = new Texture(CONTEXT+"arrow_head.png", 32, 32);
+        public static final Texture LINE = new Texture(CONTEXT+"line.png", 32, 32);
+        public static final Texture CORNER_1X1 = new Texture(CONTEXT+"corner_1x1.png", 32, 32);
+        public static final Texture CORNER_3X3 = new Texture(CONTEXT+"corner_3x3.png", 96, 96);
     }
 }
