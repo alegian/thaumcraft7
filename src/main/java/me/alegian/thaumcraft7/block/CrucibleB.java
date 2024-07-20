c1e3c2fd57d362ab0afc0f711e9afca8a6eab370
alegian <alegiannx@gmail.com>
2024-07-20 21:03:56 +0300

    restore throwing items in

--- src/main/java/me/alegian/thaumcraft7/block/CrucibleB.java
+++ src/main/java/me/alegian/thaumcraft7/block/CrucibleB.java
@@ -2,10 +2,15 @@ package me.alegian.thaumcraft7.block;
 
 import me.alegian.thaumcraft7.blockentity.CrucibleBE;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -13,11 +18,28 @@ import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.neoforge.fluids.FluidUtil;
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import org.jetbrains.annotations.Nullable;
 
 public class CrucibleB extends Block implements EntityBlock {
+    public static final VoxelShape CRUCIBLE_INSIDE = box(2.0, 4.0, 2.0, 14.0, 16.0, 14.0);
+    public static final VoxelShape CRUCIBLE_SHAPE = Shapes.join(
+            Shapes.block(),
+            Shapes.or(
+                    box(0.0, 0.0, 4.0, 16.0, 3.0, 12.0),
+                    box(4.0, 0.0, 0.0, 12.0, 3.0, 16.0),
+                    box(2.0, 0.0, 2.0, 14.0, 3.0, 14.0),
+                    CRUCIBLE_INSIDE
+            ),
+            BooleanOp.ONLY_FIRST
+    );
 
     public CrucibleB() {
         super(BlockBehaviour.Properties.ofFullCopy(Blocks.CAULDRON));
@@ -25,13 +47,33 @@ public class CrucibleB extends Block implements EntityBlock {
 
     @Override
     protected ItemInteractionResult useItemOn(ItemStack pStack, BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHitResult) {
-        ItemStack held = pPlayer.getItemInHand(pHand);
-
         if (FluidUtil.interactWithFluidHandler(pPlayer, pHand, pLevel, pPos, pHitResult.getDirection())) {
             return ItemInteractionResult.SUCCESS;
         }
 
         return super.useItemOn(pStack, pState, pLevel, pPos, pPlayer, pHand, pHitResult);
+    }
+
+    @Override
+    protected void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
+        if (!pLevel.isClientSide && pEntity instanceof ItemEntity && this.isEntityInsideContent(pPos, pEntity)) {
+            if (pEntity.mayInteract(pLevel, pPos)) {
+                pEntity.kill();
+                pLevel.playSound(pEntity, pPos, SoundEvents.GENERIC_SPLASH, SoundSource.BLOCKS, 1F, 1.0F);
+                lowerFillLevel(pLevel, pPos);
+            }
+        }
+    }
+
+    public static void lowerFillLevel(Level pLevel, BlockPos pPos) {
+        FluidUtil.getFluidHandler(pLevel, pPos, null).map(handler ->
+                    handler.drain(200, IFluidHandler.FluidAction.EXECUTE
+                ));
+    }
+
+    protected boolean isEntityInsideContent(BlockPos pPos, Entity pEntity) {
+        return pEntity.getY() < (double)pPos.getY() + 15/16f
+                && pEntity.getBoundingBox().maxY > (double)pPos.getY() + 0.25;
     }
 
     @Override
@@ -50,5 +92,20 @@ public class CrucibleB extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
         return new CrucibleBE(pPos, pState);
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return CRUCIBLE_SHAPE;
+    }
+
+    @Override
+    protected VoxelShape getInteractionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
+        return CRUCIBLE_INSIDE;
+    }
+
+    @Override
+    protected boolean isPathfindable(BlockState pState, PathComputationType pPathComputationType) {
+        return false;
     }
 }
