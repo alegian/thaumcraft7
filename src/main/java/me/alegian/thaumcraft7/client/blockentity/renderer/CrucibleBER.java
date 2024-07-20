f8aa70f8f47c7d79a5a5360754a26f5dc29d75da
alegian <alegiannx@gmail.com>
2024-07-20 20:42:01 +0300

    fix water too high

--- src/main/java/me/alegian/thaumcraft7/client/blockentity/renderer/CrucibleBER.java
+++ src/main/java/me/alegian/thaumcraft7/client/blockentity/renderer/CrucibleBER.java
@@ -8,7 +8,6 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.util.FastColor;
 import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.level.material.Fluids;
 import net.neoforged.api.distmarker.Dist;
@@ -38,13 +37,14 @@ public class CrucibleBER implements BlockEntityRenderer<CrucibleBE> {
         pPoseStack.popPose();
     }
 
-    private static void renderWaterQuad(PoseStack.Pose pose, VertexConsumer buffer, float height, int packedLight) {
+    private static void renderWaterQuad(PoseStack.Pose pose, VertexConsumer buffer, float percent, int packedLight) {
         IClientFluidTypeExtensions waterClientExtensions = IClientFluidTypeExtensions.of(Fluids.WATER);
 
         var sprite = getFluidSprite(waterClientExtensions);
         int color = waterClientExtensions.getTintColor();
 
         float width = 1f;
+        float height = 3/16f + 12/16f * percent;
 
         float minU = sprite.getU0();
         float maxU = sprite.getU1();
