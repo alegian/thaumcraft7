ea7183c588da4f8dc00c9df2fb2fd134c25b46f7
alegian <alegiannx@gmail.com>
2024-07-26 22:31:49 +0300

    groundwork for aspect particle

--- src/main/java/me/alegian/thaumcraft7/impl/client/particle/AspectsParticle.java
+++ src/main/java/me/alegian/thaumcraft7/impl/client/particle/AspectsParticle.java
@@ -1,12 +1,18 @@
 package me.alegian.thaumcraft7.impl.client.particle;
 
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import me.alegian.thaumcraft7.impl.client.TCParticleRenderTypes;
+import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 
 @OnlyIn(Dist.CLIENT)
 public class AspectsParticle extends TextureSheetParticle {
@@ -18,7 +24,7 @@ public class AspectsParticle extends TextureSheetParticle {
     super(pLevel, pX, pY, pZ);
     this.gravity = 0;
     this.lifetime = Integer.MAX_VALUE;
-    this.quadSize = .4f;
+    this.quadSize = .6f;
     this.pickSprite(spriteSet);
   }
 
@@ -31,6 +37,58 @@ public class AspectsParticle extends TextureSheetParticle {
       instance.z = pZ;
     }
     return instance;
+  }
+
+  @Override
+  public void render(VertexConsumer pBuffer, Camera pRenderInfo, float pPartialTicks) {
+    Quaternionf quaternionf = new Quaternionf();
+    this.getFacingCameraMode().setRotation(quaternionf, pRenderInfo, pPartialTicks);
+    if (this.roll != 0.0F) {
+      quaternionf.rotateZ(Mth.lerp(pPartialTicks, this.oRoll, this.roll));
+    }
+
+    this.renderOffsetRotatedQuad(pBuffer, pRenderInfo, quaternionf, 0, 0);
+    this.renderOffsetRotatedQuad(pBuffer, pRenderInfo, quaternionf, 1, 0);
+  }
+
+  public void renderOffsetRotatedQuad(VertexConsumer pBuffer, Camera pCamera, Quaternionf pQuaternion, float xOffset, float yOffset) {
+    Vec3 vec3 = pCamera.getPosition();
+    float f = (float)(this.x - vec3.x());
+    float f1 = (float)(this.y - vec3.y());
+    float f2 = (float)(this.z - vec3.z());
+    this.renderOffsetRotatedQuad(pBuffer, pQuaternion, f, f1, f2, xOffset, yOffset);
+  }
+
+  public void renderOffsetRotatedQuad(VertexConsumer pBuffer, Quaternionf pQuaternion, float pX, float pY, float pZ, float xOffset, float yOffset) {
+    float f = this.getQuadSize(1);
+    float f1 = this.getU0();
+    float f2 = this.getU1();
+    float f3 = this.getV0();
+    float f4 = this.getV1();
+    this.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, .5F + xOffset, -.5F + yOffset, f, f2, f4);
+    this.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, .5F + xOffset, .5F + yOffset, f, f2, f3);
+    this.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, -.5F + xOffset, .5F + yOffset, f, f1, f3);
+    this.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, -.5F + xOffset, -.5F + yOffset, f, f1, f4);
+  }
+
+  public void renderVertex(
+      VertexConsumer pBuffer,
+      Quaternionf pQuaternion,
+      float pX,
+      float pY,
+      float pZ,
+      float pXOffset,
+      float pYOffset,
+      float pQuadSize,
+      float pU,
+      float pV
+  ) {
+    int pPackedLight = this.getLightColor(1);
+    Vector3f vector3f = new Vector3f(pXOffset, pYOffset, 0.0F).rotate(pQuaternion).mul(pQuadSize).add(pX, pY, pZ);
+    pBuffer.addVertex(vector3f.x(), vector3f.y(), vector3f.z())
+        .setUv(pU, pV)
+        .setColor(this.rCol, this.gCol, this.bCol, this.alpha)
+        .setLight(pPackedLight);
   }
 
   @Override
