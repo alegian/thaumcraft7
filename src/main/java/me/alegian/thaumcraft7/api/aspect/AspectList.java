88577af596453462bb17f747b2cff09169628d35
alegian <alegiannx@gmail.com>
2024-07-31 01:23:47 +0300

    include stream logic inside codecs

--- src/main/java/me/alegian/thaumcraft7/api/aspect/AspectList.java
+++ src/main/java/me/alegian/thaumcraft7/api/aspect/AspectList.java
@@ -2,13 +2,16 @@ package me.alegian.thaumcraft7.api.aspect;
 
 import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.nbt.Tag;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class AspectList {
@@ -28,6 +31,27 @@ public class AspectList {
   );
 
   public static final Codec<List<Pair<Aspect, Integer>>> CODEC = PAIR_CODEC.listOf();
+
+  public static final Codec<Map<Aspect, Integer>> MAP_CODEC = new Codec<>() {
+    @Override
+    public <T> DataResult<Pair<Map<Aspect, Integer>, T>> decode(DynamicOps<T> dynamicOps, T t) {
+      var optionalListOfPairs = CODEC.parse(dynamicOps, t)
+          .resultOrPartial(System.out::println);
+
+      return optionalListOfPairs.map(o ->
+              o.stream().collect(Collectors.toMap(Pair::getFirst, Pair::getSecond))
+          )
+          .map(m -> new Pair<>(m, t))
+          .map(DataResult::success)
+          .orElse(DataResult.success(new Pair<>(new LinkedHashMap<>(), t)));
+    }
+
+    @Override
+    public <T> DataResult<T> encode(Map<Aspect, Integer> map, DynamicOps<T> dynamicOps, T t) {
+      var listOfPairs = map.entrySet().stream().map(e -> Pair.of(e.getKey(), e.getValue())).toList();
+      return CODEC.encode(listOfPairs, dynamicOps, t);
+    }
+  };
 
   public AspectList add(Aspect aspect, int amount) {
     map.compute(aspect, (k, v) -> v == null ? amount : v + amount);
@@ -51,17 +75,15 @@ public class AspectList {
   }
 
   public Tag save(HolderLookup.Provider lookupProvider) {
-    var listOfPairs = this.map.entrySet().stream().map(e -> Pair.of(e.getKey(), e.getValue())).toList();
-    return CODEC.encodeStart(lookupProvider.createSerializationContext(NbtOps.INSTANCE), listOfPairs).getOrThrow();
+    return MAP_CODEC.encodeStart(lookupProvider.createSerializationContext(NbtOps.INSTANCE), this.map)
+        .getOrThrow();
   }
 
   public static AspectList parse(HolderLookup.Provider lookupProvider, Tag tag) {
-    var optionalListOfPairs = CODEC.parse(lookupProvider.createSerializationContext(NbtOps.INSTANCE), tag)
+    var optionalMap = MAP_CODEC.parse(lookupProvider.createSerializationContext(NbtOps.INSTANCE), tag)
         .resultOrPartial(System.out::println);
 
-    return optionalListOfPairs.map(o ->
-        new AspectList(o.stream().collect(Collectors.toMap(Pair::getFirst, Pair::getSecond)))
-    ).orElse(new AspectList());
+    return optionalMap.map(AspectList::new).orElse(new AspectList());
   }
 
   @Override
