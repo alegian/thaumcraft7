f754c1efe08322149fb21b64bc74d76659dd2ccd
alegian <alegiannx@gmail.com>
2024-08-05 15:33:34 +0300

    working custom shader

--- src/main/java/me/alegian/thaumcraft7/impl/client/T7BufferBuilder.java
+++ src/main/java/me/alegian/thaumcraft7/impl/client/T7BufferBuilder.java
@@ -4,30 +4,38 @@ import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.world.phys.Vec3;
-import net.neoforged.neoforge.client.model.pipeline.VertexConsumerWrapper;
 import org.jetbrains.annotations.NotNull;
+import org.lwjgl.system.MemoryUtil;
 
 public class T7BufferBuilder {
   private Vec3 currentPosition;
-  private BufferBuilder parent;
+  private final BufferBuilder parent;
 
   public T7BufferBuilder(VertexConsumer parent) {
-    if(!(parent instanceof BufferBuilder)) throw new IllegalArgumentException("Thaumcraft Exception: parent must be a BufferBuilder");
+    if (!(parent instanceof BufferBuilder))
+      throw new IllegalArgumentException("Thaumcraft Exception: parent must be a BufferBuilder");
     this.parent = (BufferBuilder) parent;
   }
 
   public @NotNull T7BufferBuilder setColor(float pRed, float pGreen, float pBlue, float pAlpha) {
-    return (T7BufferBuilder) parent.setColor(pRed, pGreen, pBlue, pAlpha);
+    parent.setColor(pRed, pGreen, pBlue, pAlpha);
+    return this;
   }
 
   public @NotNull T7BufferBuilder addVertex(PoseStack.@NotNull Pose pPose, float pX, float pY, float pZ) {
     currentPosition = new Vec3(pX, pY, pZ);
-    return (T7BufferBuilder) parent.addVertex(pPose, pX, pY, pZ);
+    parent.addVertex(pPose, pX, pY, pZ);
+    return this;
   }
 
   // assumes our pose is centered at the block center
   public T7BufferBuilder setRadius() {
     float radius = (float) currentPosition.length();
-    return (T7BufferBuilder) misc(T7VertexFormats.RADIUS, Float.floatToIntBits(radius));
+    long i = parent.beginElement(T7VertexFormats.RADIUS);
+    if (i != -1L) {
+      MemoryUtil.memPutFloat(i, radius);
+    }
+
+    return this;
   }
 }
