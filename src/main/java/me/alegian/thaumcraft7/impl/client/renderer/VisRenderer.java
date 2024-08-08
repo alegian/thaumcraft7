6d5169fd280a20cc8c3acc0ff6a721cc04a89e28
alegian <alegiannx@gmail.com>
2024-08-09 01:09:27 +0300

    smooth sine

--- src/main/java/me/alegian/thaumcraft7/impl/client/renderer/VisRenderer.java
+++ src/main/java/me/alegian/thaumcraft7/impl/client/renderer/VisRenderer.java
@@ -22,16 +22,25 @@ public class VisRenderer {
     poseStack.translate(-camera.getPosition().x, -camera.getPosition().y, -camera.getPosition().z);
 
     for (int i = 0; i < N; i++) {
-      float dy = (float) Math.sin((double) i / N * Math.PI * 2);
       renderQuad(bufferSource, poseStack.last(),
-          offsetY(add(a, offsetY(mul(dx, i), dy)), 0),
-          offsetY(add(a, offsetY(mul(dx, i + 1), dy)), 0),
-          offsetY(add(a, offsetY(mul(dx, i + 1), dy)), 1),
-          offsetY(add(a, offsetY(mul(dx, i), dy)), 1)
+          offsetY(add(a, offset(mul(dx, i), i)), 0),
+          offsetY(add(a, offset(mul(dx, i + 1), i+1)), 0),
+          offsetY(add(a, offset(mul(dx, i + 1), i+1)), 1),
+          offsetY(add(a, offset(mul(dx, i), i)), 1)
       );
     }
 
     poseStack.popPose();
+  }
+
+  private static Vector3f offset(Vector3f v1, int i) {
+    Vector3f offset = calculateOffset(i);
+    return new Vector3f(v1).add(offset);
+  }
+
+  private static Vector3f calculateOffset(int i) {
+    float Y = (float) Math.sin((double) i / N * Math.PI * 2);
+    return new Vector3f(0, Y, 0);
   }
 
   private static Vector3f offsetY(Vector3f v1, float offset) {
