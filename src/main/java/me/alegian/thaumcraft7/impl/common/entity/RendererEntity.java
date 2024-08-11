0d247eb6b2f50893f77dc920852374f24f97e7d3
alegian <alegiannx@gmail.com>
2024-08-11 13:21:00 +0300

    add comment

--- src/main/java/me/alegian/thaumcraft7/impl/common/entity/RendererEntity.java
+++ src/main/java/me/alegian/thaumcraft7/impl/common/entity/RendererEntity.java
@@ -9,6 +9,7 @@ import net.minecraft.world.phys.Vec3;
 
 // helper entity used for spawning Renderers
 public class RendererEntity extends Entity {
+  // position is used to determine if the render section is active, otherwise the entity is culled
   public RendererEntity(Level pLevel, Vec3 pos) {
     super(T7EntityTypes.RENDERER.get(), pLevel);
     setPos(pos);
