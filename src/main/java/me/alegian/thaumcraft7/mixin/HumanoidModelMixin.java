1240837573570ebfbb02a64f7ec6c2099753fb77
alegian <alegiannx@gmail.com>
2024-09-09 23:43:00 +0300

    try making mixin

---
+++ src/main/java/me/alegian/thaumcraft7/mixin/HumanoidModelMixin.java
@@ -0,0 +1,33 @@
+package me.alegian.thaumcraft7.mixin;
+
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = HumanoidModel.class, remap = false)
+public class HumanoidModelMixin {
+  @Inject(
+      method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V",
+      at = @At(value = "INVOKE",
+          target = "Lnet/minecraft/client/model/HumanoidModel;setupAttackAnimation(Lnet/minecraft/world/entity/LivingEntity;F)V"
+      )
+  )
+  private void thaumcraft7_setupKatanaAttackAnimationOffhand(LivingEntity pLivingEntity, float pLimbSwing, float pLimbSwingAmount, float pAgeInTicks, float pNetHeadYaw, float pHeadPitch, CallbackInfo info) {
+    test(pLivingEntity, pAgeInTicks);
+  }
+
+  private void test(LivingEntity pLivingEntity, float pAgeInTicks) {
+    pLivingEntity.swingingArm = InteractionHand.OFF_HAND;
+    setupAttackAnimation(pLivingEntity, pAgeInTicks);
+    pLivingEntity.swingingArm = InteractionHand.MAIN_HAND;
+  }
+
+  @Shadow
+  private void setupAttackAnimation(LivingEntity pLivingEntity, float pAgeInTicks) {
+  }
+}
