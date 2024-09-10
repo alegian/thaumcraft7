14b4a4047e4db9ea4bcf357c200cd3c06710fb5e
alegian <alegiannx@gmail.com>
2024-09-10 19:01:27 +0300

    second part of dual wield mixin

---
+++ src/main/java/me/alegian/thaumcraft7/mixin/ItemInHandRendererMixin.java
@@ -0,0 +1,35 @@
+package me.alegian.thaumcraft7.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.mojang.blaze3d.vertex.PoseStack;
+import me.alegian.thaumcraft7.impl.common.entity.EntityHelper;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.ItemInHandRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(value = ItemInHandRenderer.class, remap = false)
+public abstract class ItemInHandRendererMixin {
+  @WrapMethod(method = "renderArmWithItem(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V")
+  private void thaumcraft7_renderArmWithItemWrapper(
+      AbstractClientPlayer player,
+      float pPartialTicks,
+      float pPitch,
+      InteractionHand pHand,
+      float pSwingProgress,
+      ItemStack pStack,
+      float pEquippedProgress,
+      PoseStack pPoseStack,
+      MultiBufferSource pBuffer,
+      int pCombinedLight,
+      Operation<Void> original
+  ) {
+    if (EntityHelper.isHandKatana(InteractionHand.MAIN_HAND) && EntityHelper.isHandKatana(InteractionHand.OFF_HAND)) {
+      pSwingProgress = player.getAttackAnim(pPartialTicks);
+    }
+    original.call(player, pPartialTicks, pPitch, pHand, pSwingProgress, pStack, pEquippedProgress, pPoseStack, pBuffer, pCombinedLight);
+  }
+}
