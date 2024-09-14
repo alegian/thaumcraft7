f1b1ca676f797e0dcb83e2788fa405276fac4dd4
alegian <alegiannx@gmail.com>
2024-09-14 20:46:16 +0300

    new saplings, better leaves

--- src/main/java/me/alegian/thaumcraft7/impl/client/T7Colors.java
+++ src/main/java/me/alegian/thaumcraft7/impl/client/T7Colors.java
@@ -5,6 +5,6 @@ import net.neoforged.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class T7Colors {
-  public static final int GREATWOOD_LEAVES = 0x0C4409;
-  public static final int SILVERWOOD_LEAVES = 0x00BBFF;
+  public static final int GREATWOOD_LEAVES = 0x041B03;
+  public static final int SILVERWOOD_LEAVES = 0x007FAD;
 }
