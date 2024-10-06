8e736bf8fb653ea473a8473af944063918b01e26
alegian <alegiannx@gmail.com>
2024-10-06 12:44:24 +0300

    bump all versions

--- src/main/java/me/alegian/thaumcraft7/impl/init/registries/deferred/T7DataComponents.java
+++ src/main/java/me/alegian/thaumcraft7/impl/init/registries/deferred/T7DataComponents.java
@@ -3,12 +3,13 @@ package me.alegian.thaumcraft7.impl.init.registries.deferred;
 import me.alegian.thaumcraft7.impl.Thaumcraft;
 import me.alegian.thaumcraft7.impl.common.aspect.AspectList;
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
 
 public class T7DataComponents {
-  public static final DeferredRegister.DataComponents REGISTRAR = DeferredRegister.createDataComponents(Thaumcraft.MODID);
+  public static final DeferredRegister.DataComponents REGISTRAR = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, Thaumcraft.MODID);
 
   public static final Supplier<DataComponentType<AspectList>> ASPECTS = REGISTRAR.registerComponentType("aspects",
       (builder) -> builder
