240f3baa03f80b08d052323a3a42ce82c60e5c10
alegian <alegiannx@gmail.com>
2024-12-01 21:03:57 +0200

    kinda fix emissive matrix

--- src/main/java/me/alegian/thavma/impl/client/renderer/blockentity/MatrixBER.java
+++ src/main/java/me/alegian/thavma/impl/client/renderer/blockentity/MatrixBER.java
@@ -2,8 +2,12 @@ package me.alegian.thavma.impl.client.renderer.blockentity;
 
 import me.alegian.thavma.impl.Thavma;
 import me.alegian.thavma.impl.common.block.entity.MatrixBE;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.cache.texture.AutoGlowingTexture;
 import software.bernie.geckolib.model.DefaultedBlockGeoModel;
 import software.bernie.geckolib.renderer.GeoBlockRenderer;
 import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
@@ -12,6 +16,11 @@ import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 public class MatrixBER extends GeoBlockRenderer<MatrixBE> {
   public MatrixBER() {
     super(new DefaultedBlockGeoModel<>(Thavma.rl("infusion_matrix")));
-    this.addRenderLayer(new AutoGlowingGeoLayer<>(this));
+    this.addRenderLayer(new AutoGlowingGeoLayer<>(this) {
+      @Override
+      protected RenderType getRenderType(MatrixBE animatable, @Nullable MultiBufferSource bufferSource) {
+        return RenderType.eyes(AutoGlowingTexture.getEmissiveResource(this.getTextureResource(animatable)));
+      }
+    });
   }
 }
