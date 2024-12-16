da495da809dd1e73fd530f7bdab6bb06fb7c08d5
alegian <alegiannx@gmail.com>
2024-12-16 19:11:34 +0200

    disallow placement if obstructed

--- src/main/java/me/alegian/thavma/impl/common/block/PillarBlock.java
+++ src/main/java/me/alegian/thavma/impl/common/block/PillarBlock.java
@@ -4,32 +4,39 @@ import me.alegian.thavma.impl.common.block.entity.PillarBE;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class PillarBlock extends Block implements EntityBlock {
-  public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
-
   public PillarBlock() {
     super(Properties.ofFullCopy(Blocks.STONE).noOcclusion());
     this.registerDefaultState(this.stateDefinition.any()
-        .setValue(PillarBlock.FACING, Direction.NORTH)
+        .setValue(BlockStateProperties.HORIZONTAL_FACING, Direction.NORTH)
     );
   }
 
   @Override
   public BlockState getStateForPlacement(BlockPlaceContext context) {
-    return this.defaultBlockState().setValue(PillarBlock.FACING, context.getHorizontalDirection());
+    var stateForPlacement = this.defaultBlockState().setValue(BlockStateProperties.HORIZONTAL_FACING, context.getHorizontalDirection());
+    var pos = context.getClickedPos();
+    var facing = stateForPlacement.getValue(BlockStateProperties.HORIZONTAL_FACING);
+
+    if (!context.getLevel().getBlockState(pos.relative(facing)).canBeReplaced()) return null;
+    if (!context.getLevel().getBlockState(pos.relative(facing).above()).canBeReplaced()) return null;
+    return stateForPlacement;
   }
 
   @Override
   protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-    builder.add(PillarBlock.FACING);
+    builder.add(BlockStateProperties.HORIZONTAL_FACING);
   }
 
   @NotNull
