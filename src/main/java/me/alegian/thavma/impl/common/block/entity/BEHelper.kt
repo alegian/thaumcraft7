5a08f38e4efd3ecb9b9feb9b9028b1712d2ea400
alegian <alegiannx@gmail.com>
2024-12-19 22:43:11 +0200

    port aura node be

--- src/main/java/me/alegian/thavma/impl/common/block/entity/BEHelper.kt
+++ src/main/java/me/alegian/thavma/impl/common/block/entity/BEHelper.kt
@@ -4,10 +4,10 @@ import net.minecraft.core.BlockPos
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 
-fun updateServerBlockEntity(level: Level, blockPos: BlockPos) {
-    level.getBlockEntity(blockPos)?.let { be ->
+fun Level.updateServerBlockEntity(blockPos: BlockPos) {
+    getBlockEntity(blockPos)?.let { be ->
         be.setChanged()
-        level.sendBlockUpdated(blockPos, be.blockState, be.blockState, Block.UPDATE_CLIENTS)
+        sendBlockUpdated(blockPos, be.blockState, be.blockState, Block.UPDATE_CLIENTS)
     }
 }
 
