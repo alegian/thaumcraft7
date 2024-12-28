d1c67019b6a86cac9cf1b77a7e64f3a0fd135823
alegian <alegiannx@gmail.com>
2024-12-28 16:21:50 +0200

    trajectory

--- src/main/java/me/alegian/thavma/impl/client/renderer/level/EssentiaRenderer.kt
+++ src/main/java/me/alegian/thavma/impl/client/renderer/level/EssentiaRenderer.kt
@@ -6,15 +6,36 @@ import me.alegian.thavma.impl.client.util.addVertex
 import me.alegian.thavma.impl.client.util.setColorDebug
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.core.BlockPos
+import net.minecraft.world.phys.Vec3
+import thedarkcolour.kotlinforforge.neoforge.forge.vectorutil.v3d.div
+import thedarkcolour.kotlinforforge.neoforge.forge.vectorutil.v3d.minus
+import thedarkcolour.kotlinforforge.neoforge.forge.vectorutil.v3d.plus
+import thedarkcolour.kotlinforforge.neoforge.forge.vectorutil.v3d.times
+
+fun trajectory(start: Vec3, end: Vec3): List<Vec3> {
+    val dl = (end - start) / 10.0
+    return (0..10).map { start + dl * it.toDouble() }
+}
 
 fun renderEssentia(startPos: BlockPos, endPos: BlockPos, poseStack: PoseStack, multiBufferSource: MultiBufferSource) {
     val vc = multiBufferSource.getBuffer(T7RenderTypes.TRANSLUCENT_TRIANGLES)
     val start = startPos.center
     val end = endPos.center
 
-    vc.addVertex(poseStack, start).setColorDebug()
-    vc.addVertex(poseStack, end).setColorDebug()
-    vc.addVertex(poseStack, end.add(1.0, 0.0, 0.0)).setColorDebug()
-    vc.addVertex(poseStack, start.add(1.0, 0.0, 0.0)).setColorDebug()
-    vc.addVertex(poseStack, start).setColorDebug()
+    trajectory(start, end).run {
+        for (i in 0 until size - 1) {
+            val currentPoint = this[i]
+            val nextPoint = this[i + 1]
+
+            val direction = (nextPoint - currentPoint).normalize()
+            val randomOtherDirection = (direction - Vec3(0.0, 1.0, 0.0)).normalize()
+            val normal1 = direction.cross(randomOtherDirection)
+            val normal2 = direction.cross(normal1)
+
+            vc.addVertex(poseStack, currentPoint).setColorDebug()
+            //vc.addVertex(poseStack, nextPoint).setColorDebug()
+            vc.addVertex(poseStack, currentPoint + normal2).setColorDebug()
+            //vc.addVertex(poseStack, nextPoint + normal2).setColorDebug()
+        }
+    }
 }
