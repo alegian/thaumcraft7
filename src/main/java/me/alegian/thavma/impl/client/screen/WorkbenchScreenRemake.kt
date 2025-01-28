285797e9c94dac7ecdc2946d95b1834fbfa4af4c
alegian <alegiannx@gmail.com>
2025-01-28 18:27:18 +0200

    add wand slot back in

--- src/main/java/me/alegian/thavma/impl/client/screen/WorkbenchScreenRemake.kt
+++ src/main/java/me/alegian/thavma/impl/client/screen/WorkbenchScreenRemake.kt
@@ -6,8 +6,9 @@ import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.player.Inventory
 
 private val WORKBENCH_BG = Texture("gui/container/arcane_workbench/bg", 214, 132, 256, 256)
-private val RESULT_SLOT_TEXTURE = Texture("gui/container/arcane_workbench/result_slot", 34, 34)
-private val ASPECT_SLOT_TEXTURE = Texture("gui/container/arcane_workbench/aspect_slot", 25, 25)
+private val RESULT_SLOT = Texture("gui/container/arcane_workbench/result_slot", 34, 34)
+private val WAND_SLOT = Texture("gui/container/arcane_workbench/wand_slot", 20, 20)
+private val ASPECT_SOCKET = Texture("gui/container/arcane_workbench/aspect_socket", 25, 25)
 private val SLOTS = (0..8).map { Texture("gui/container/arcane_workbench/crafting_slots/$it", 21, 21) }
 private const val BORDER = 5
 private const val WOOD_SIZE = 122
@@ -22,9 +23,16 @@ open class WorkbenchScreenRemake(val menu: WorkbenchMenu, pPlayerInventory: Inve
           }
         }
         Box(Modifier().width(BORDER))
-        Box(Modifier().center()) {
-          Box(Modifier().size(RESULT_SLOT_TEXTURE)){
-            addRenderableOnly(slot(menu.resultContainer.range.slot, RESULT_SLOT_TEXTURE))
+        Box {
+          Box(Modifier().center()){
+            Box(Modifier().size(RESULT_SLOT)){
+              addRenderableOnly(slot(menu.resultContainer.range.slot, RESULT_SLOT))
+            }
+          }
+          Box(Modifier().maxHeight(0.5f).center()){
+            Box(Modifier().size(WAND_SLOT)){
+              addRenderableOnly(slot(menu.wandContainer.range.slot, WAND_SLOT))
+            }
           }
         }
       }
